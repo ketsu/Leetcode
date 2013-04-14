a73ff9e903c19b8265f58dc34557e83daab800b3
ketsu <yinketsu@gmail.com>
2013-04-13 20:05:08 -0700

    Update Reverse_Integer.cpp

--- Reverse_Integer.cpp
+++ Reverse_Integer.cpp
@@ -8,20 +8,21 @@ public:
     int reverse(int x) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        int isneg = 0;
+        //int isneg = 0;// A negative flag is not actually needed
         int res = 0;
   	//when x == 0
 		if(!x) return x;
 		//when x is negative
-		if(x < 0) {
+		/*if(x < 0) {
 			isneg = 1;
 			x = -x;
-		}
+		}*/
 		//reverse the integer
 		while(x){
 			res = 10*res + x%10;
 			x /= 10;
 		}
-        return (isneg==0)? res: -res;
+        //return (isneg==0)? res: -res;
+        return x;
     }
 };
